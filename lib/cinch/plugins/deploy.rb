575d73de11404ab3cb33c90bef85e3402d34657d
Michal Papis <mpapis@gmail.com>
2013-11-26 17:23:13 +0100

    switch back to automatically using user in messages

--- lib/cinch/plugins/deploy.rb
+++ lib/cinch/plugins/deploy.rb
@@ -12,14 +12,13 @@ module Cinch
           if config[:channels].include?(m.channel.to_s) and config[:users].include?(m.user.nick) and m.message =~ Regexp.new(config[:trigger])
             IO.popen("#{config[:command]} 2>&1") do |handle|
               while line = handle.gets
-                # do not use ,true for nick, it breaks on split
-                m.reply "#{m.user.nick}: #{config[:command]} > #{line}"
+                m.reply "#{config[:command]} > #{line}", true
               end
             end
           end
         end
       rescue Exception => e
-        m.reply "#{config[:command]} - exception: #{e.message}"
+        m.reply "#{config[:command]} - exception: #{e.message}", true
         puts e.backtrace
       ensure
         @running = nil
