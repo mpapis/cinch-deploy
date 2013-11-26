0fb1164afaf0311de89b736d0955ddee6e964a44
Michal Papis <mpapis@gmail.com>
2013-11-26 16:01:15 +0100

    improved logging and exceptions handling

--- lib/cinch/plugins/deploy.rb
+++ lib/cinch/plugins/deploy.rb
@@ -12,15 +12,14 @@ module Cinch
           if config[:channels].include?(m.channel.to_s) and config[:users].include?(m.user.nick) and m.message =~ Regexp.new(config[:trigger])
             IO.popen("#{config[:command]} 2>&1") do |handle|
               while line = handle.gets
-                m.reply line, true
+                m.reply "#{config[:command]} > #{line}", true
               end
             end
-          else
-            m.reply "ch:#{m.channel.to_s} usr:#{m.user.nick} msg:#{m.message}", true
           end
         end
       rescue Exception => e
-        m.reply "exception: #{e.message}"
+        m.reply "#{config[:command]} - exception: #{e.message}"
+        puts e.backtrace
       ensure
         @running = nil
       end
